b6767fba88167646fce374a0ac76f22c18434d34
kitety <aizaizuori@foxmail.com>
2019-02-13 22:46:09 +0800

    数据的关联

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,23 +1,63 @@
 const graphql = require("graphql");
 const _ = require("lodash");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList
+} = require("graphql");
+
+/**
+ * 书 作者 一个书一个作者(假设) 一个作者很多书
+ * 查询一个作者的书
+ * 查询一本书的作者
+ */ 
+
 
 const books = [
   {
     name: "深入react技术栈",
     genre: "计算机",
-    id: "1"
+    id: "1",
+    authorId: "1"
+  },
+  {
+    name: "深入react技术栈1",
+    genre: "计算机1",
+    id: "11",
+    authorId: "1"
   },
   {
     name: "自我",
     genre: "心理",
-    id: "2"
+    id: "2",
+    authorId: "2"
+  },
+  {
+    name: "自我1",
+    genre: "心理1",
+    id: "12",
+    authorId: "2"
   },
   {
     name: "CSS世界",
     genre: "计算机",
-    id: "3"
+    id: "3",
+    authorId: "3"
+  },
+  {
+    name: "CSS世界1",
+    genre: "计算机1",
+    id: "13",
+    authorId: "3"
   }
+];
+const authors = [
+  { name: "kitety1", age: 22, id: "1" },
+  { name: "kitety2", age: 23, id: "2" },
+  { name: "kitety3", age: 24, id: "3" }
 ];
 // console.log(_.find(books, { id: '2' }));
 
@@ -25,15 +65,31 @@ const books = [
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    // 数据的关联
+    author:{
+      type: AuthorType,
+      resolve(parent,args){
+      // parent对应查询到的对象
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
   })
 });
 const AuthorType = new GraphQLObjectType({
   name: "Author",
   fields: () => ({
-    name: { type: GraphQLString }
+    id: { type: GraphQLID },
+    age: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    books:{
+      type: new GraphQLList(BookType),
+      resolve(parent,args){
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
   })
 });
 const RootQuery = new GraphQLObjectType({
@@ -44,14 +100,30 @@ const RootQuery = new GraphQLObjectType({
       // 参数
       args: {
         id: {
-          type: GraphQLString
+          type: GraphQLID
         }
       },
       resolve(parent, args) {
-        console.log(args)
+        // 传递整型也会转换为string
+        console.log(args);
         // 数据来源,从哪里得到数据:数据库或者其他来源
         // mongodb mysql postgresql
         return _.find(books, { id: args.id });
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: GraphQLID
+        }
+      },
+      resolve(parent, args) {
+        // 传递整型也会转换为string
+        console.log(args);
+        // 数据来源,从哪里得到数据:数据库或者其他来源
+        // mongodb mysql postgresql
+        return _.find(authors, { id: args.id });
       }
     }
   }
