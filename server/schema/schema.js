51e815f99607ca6d26d2c6f902401555577ae44b
kitety <aizaizuori@foxmail.com>
2019-02-13 20:53:59 +0800

    resolve function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,25 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+
+const books = [
+  {
+    name: "深入react技术栈",
+    genre: "计算机",
+    id: "1"
+  },
+  {
+    name: "自我",
+    genre: "心理",
+    id: "2"
+  },
+  {
+    name: "CSS世界",
+    genre: "计算机",
+    id: "3"
+  }
+];
+// console.log(_.find(books, { id: '2' }));
 
 // book author
 const BookType = GraphQLObjectType({
@@ -29,6 +49,8 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve(parent, args) {
         // 数据来源,从哪里得到数据:数据库或者其他来源
+        // mongodb mysql postgresql
+        _.find(books, { id: args.id });
       }
     }
   }
