29f11c315b7d2a8cc3e52e894b669a594e8d9909
kitety <aizaizuori@foxmail.com>
2019-02-28 23:51:04 +0800

    引入apollo客户端

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
-import BookList from './components/BookList'
+import BookList from "./components/BookList";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+
+// 客户端
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Reading List</h1>
-        <BookList/>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
